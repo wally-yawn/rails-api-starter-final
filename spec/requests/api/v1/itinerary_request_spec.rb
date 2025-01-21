0b381aa4e784ae6b0b22c781041fd7f77f8453a6
Wally Wallace <jrvw001@gmail.com>
2025-01-21 14:03:02 -0700

    Finish happy path test

--- spec/requests/api/v1/itinerary_request_spec.rb
+++ spec/requests/api/v1/itinerary_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Itinerary API', type: :request do
   describe 'index' do
     it 'returns a list of itineraries' do
       Itinerary.destroy_all
-      
+
       itinerary1 = Itinerary.create!(
         title: "CO Bands Day 1",
         date: Date.new(2025, 2, 1),
@@ -12,14 +12,8 @@ RSpec.describe 'Itinerary API', type: :request do
       )
       
       itinerary2 = Itinerary.create!(
-        title: "CO Bands Day 2",
+        title: "Metal Bands Day 2",
         date: Date.new(2025, 2, 2),
-        img_url: "https://en.wikipedia.org/wiki/File:Flag_of_Colorado.svg"
-      )
-      
-      itinerary3 = Itinerary.create!(
-        title: "Metal Bands Day 1",
-        date: Date.new(2025, 2, 1),
         img_url: "https://themusicroom.me/wp-content/uploads/2014/10/Nita.jpg"
       )
       
@@ -27,6 +21,18 @@ RSpec.describe 'Itinerary API', type: :request do
       json = JSON.parse(response.body, symbolize_names: true)
       expect(response).to be_successful
 
+      expect(json[:data].length).to eq(2)
+      expect(json[:data][0][:id]).to eq(itinerary1.id)
+      expect(json[:data][0][:type]).to eq("itinerary")
+      expect(json[:data][0][:attributes][:title]).to eq("CO Bands Day 1")
+      expect(json[:data][0][:attributes][:date]).to eq("2025-02-01")
+      expect(json[:data][0][:attributes][:img_url]).to eq("https://en.wikipedia.org/wiki/File:Flag_of_Colorado.svg")
+
+      expect(json[:data][1][:id]).to eq(itinerary2.id)
+      expect(json[:data][1][:type]).to eq("itinerary")
+      expect(json[:data][1][:attributes][:title]).to eq("Metal Bands Day 2")
+      expect(json[:data][1][:attributes][:date]).to eq("2025-02-02")
+      expect(json[:data][1][:attributes][:img_url]).to eq("https://themusicroom.me/wp-content/uploads/2014/10/Nita.jpg")
 
     end
   end
